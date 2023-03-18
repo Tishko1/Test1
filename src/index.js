36619a458f9dad32e3b8638ade6151481e3b1138
Tishko1 <113176690+Tishko1@users.noreply.github.com>
2023-03-18 16:55:55 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Global} from '@emotion/react';
 import { GlobalStyles } from 'styles/GlobalStyles';
 import 'modern-normalize/modern-normalize.css';
@@ -8,9 +9,11 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   
+   <BrowserRouter
+  basename='Test1'
+  >
       <Global styles={GlobalStyles} />
       <App />
-    
+      </BrowserRouter>
   </React.StrictMode>
 );
